cf00c07f8e16c1b6d8330387d703c31d655d0c02
Jackie Gregory <jackiebgregory7@gmail.com>
2021-01-19 19:43:41 -0600

    working with arrays

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log(this is about arrays and loops)
+const students = ['Katy', 'Jackie', 'Sean', 'Nathan'];
+// console.log(students.length);
+
+// *******ACCESSING ITEMS IN ARRAY
+// console.log(students[1]); // Jackie
+// console.log(students[students.length - 1]); // Nathan
+
+//students[0] = 'Matthew';
+// console.log(students);
+
+//const valuePrinter = (array, index) => {
+//  return array[index];
+//}
+
+//console.log(valuePrinter(students, 2));
+//console.log(valuePrinter([1,2,3,4,5], 2));
+
+//Write a function that takes an array of something and tell you if the name 'Greg' is in that array
+
+// findStudent accepts an array and a string
+const findStudent = (array, student) => {
+  return array.includes(student);
+}
+console.log(findStudent(students, 'Greg')); // false
+console.log(findStudent(students, 'Dr. T')); // false
+console.log(findStudent(students, 'Nathan')); // true
